9f04f30493ee79265dc7cb84c9d1b4f927b2181c
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-02-23 15:03:04 +0100

    decreased step, modified initial position

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char* argv[]){
   double wind_speed_x=atol(argv[1]);
-  vect initial_position{pi/4, pi/4, 10};
+  vect initial_position{pi/4, pi/12, 10};
   vect initial_velocity{0, 0, 0};
   kite k{initial_position, initial_velocity};
-  k.simulate(0.01,60000,wind_speed_x);//simulating for 10 minutes
+  k.simulate(0.001,600000,wind_speed_x);//simulating for 10 minutes
 }
