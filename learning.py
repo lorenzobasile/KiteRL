d1db236d8da8355ac88e164d9b061b7e64615c80
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2021-11-16 14:46:42 +0100

    unified learning.py

---
+++ learning.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pykite as pk
+from utils import *
+import matplotlib.pyplot as plt
+import os
+from learning.deep.models import NN, NN5
+import sys
+
+filename=sys.argv[1]
+
+with open(filename) as file:
+    params={}
+    lines=file.readlines()
+    for line in lines:
+        line=line.split()
+        print(line)
+        try:
+            params[line[0]]=float(line[1])
+        except ValueError:
+            params[line[0]]=line[1]
+
+
+np.random.seed(0)
+
+path="./plots/test/"
+n_attack=pk.coefficients.shape[0]
+n_bank=pk.bank_angles.shape[0]
+n_beta=pk.n_beta
+max_power=params['max_power']
+window_size=30
+initial_position=pk.vect(np.pi/6, 0, 50)
+initial_velocity=pk.vect(0, 0, 0)
+wind_type=params['wind_type']
+k=pk.kite(initial_position, initial_velocity, wind_type)
+if params['learning_type']=='sarsa':
+    Q=np.ones((n_attack, n_bank, n_beta, 3, 3))
+    Q*=(max_power*2)
+    Q, durations, rewards=sarsa(k, Q, params, initial_position, initial_velocity)
+else:
+    net=NN()
+    net, durations, rewards=dql(k, net, params, initial_position, initial_velocity)
--- utils.py
+++ utils.py
@@ -45,8 +45,6 @@ def step(Q, S_t, A_t, R_t1, S_t1, A_t1, eta, gamma):
     Q[S_t+A_t]=Q[S_t+A_t]+eta*(R_t1+gamma*Q[S_t1+A_t1]-Q[S_t+A_t])
     return Q
 
-
-
 def scheduling(value, t, T, exp=0.6):
     if t>T:
         return value/((t-T)**exp)
@@ -77,32 +75,44 @@ def plot_trajectory(theta, phi, r, save=None, marker='-'):
         plt.savefig(save)
     plt.show()
 
-def dql(net, opt, episodes, horizon, learning_step, integration_step, integration_steps_per_learning_step, initial_pos, initial_vel, wind, lr=0.1, lr_decay_exp=0.6, lr_decay_start=200000, eps=0.01, eps_decay_exp=0.6, eps_decay_start=400000, gamma=1, plot=False):
+def dql(k, net, params, initial_position, initial_velocity):
     durations=[]
     rewards=[]
     losses=[]
-    if opt=='adam':
+    lr=params['eta0']
+    if params['optimizer']=='adam':
         optimizer=torch.optim.Adam(net.parameters(), lr=lr, weight_decay=0.000)
     else:
         optimizer=torch.optim.SGD(net.parameters(), lr=lr, weight_decay=0.000)
-    loss=torch.nn.SmoothL1Loss()
+    if params['loss']=='huber':
+        loss=torch.nn.SmoothL1Loss()
+    else:
+        loss=torch.nn.MSELoss()
     t=0
-    k=pk.kite(initial_pos, initial_vel)
+    wind_type=params['wind_type']
+    episodes=int(params['episodes'])
     for episode in range(episodes):
         print(episode)
-        k.reset(initial_pos, initial_vel)
-        t,theta,phi,r=dql_episode(k, net, optimizer, loss, initial_pos, initial_vel, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, lr, lr_decay_exp, lr_decay_start, eps, eps_decay_exp, eps_decay_start, durations, rewards, gamma, t, plot)
-    return durations, rewards,theta,phi,r
+        k.reset(initial_position, initial_velocity, wind_type)
+        duration, reward=dql_episode(k, net, optimizer, loss, params, initial_position, initial_velocity, t)
+        t+=duration
+        durations.append(duration)
+        rewards.append(reward)
+    return net, durations, rewards
 
-def dql_episode(k, net, optimizer, loss, initial_position, initial_velocity, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, eta0, eta_decay, eta_decay_start, eps0, eps_decay, eps_decay_start, durations, rewards, gamma, t, plot):
-    if plot:
-        theta=[]
-        phi=[]
-        r=[]
-    else:
-        theta=None
-        phi=None
-        r=None
+def dql_episode(k, net, optimizer, loss, params, initial_position, initial_velocity, t):
+    episode_duration=params['episode_duration']
+    learning_step=params['learning_step']
+    horizon=int(episode_duration/learning_step)
+    integration_step=params['integration_step']
+    integration_steps_per_learning_step=int(learning_step/integration_step)
+    eta0=params['eta0']
+    gamma=params['gamma']
+    eps0=params['eps0']
+    eps_exp=params['eps_decay_rate']
+    eps_start=params['eps_decay_start']
+    eta_exp=params['eta_decay_rate']
+    eta_start=params['eta_decay_start']
     cumulative_reward=0
     factor=1e5
     initial_beta=k.beta()
@@ -116,18 +126,8 @@ def dql_episode(k, net, optimizer, loss, initial_position, initial_velocity, win
     tensor_state[1]/=n_bank
     tensor_state[2]/=n_beta
     for i in range(horizon):
-        '''
-        if k.position.r*np.cos(k.position.theta)>100:
-            print(k.position.r*np.cos(k.position.theta))
-            print(k.position.r)
-            break
-        '''
-        if plot:
-            theta.append(k.position.theta)
-            phi.append(k.position.phi)
-            r.append(k.position.r)
         t+=1
-        eps=scheduling(eps0, t, eps_decay_start, exp=eps_decay)
+        eps=scheduling(eps0, t, eps_start, exp=eps_exp)
         if i<300:
             eps=eps*1
         else:
@@ -135,14 +135,12 @@ def dql_episode(k, net, optimizer, loss, initial_position, initial_velocity, win
         q=net(tensor_state).reshape(3,3)
         A_t=torch.randint(3,(2,)) if np.random.rand()<eps else (q==torch.max(q)).nonzero().reshape(-1)
         A_t=A_t[0],A_t[1]
-        optimizer.param_groups[0]['lr']=scheduling(eta0, t, eta_decay_start, exp=eta_decay)
+        optimizer.param_groups[0]['lr']=scheduling(eta0, t, eta_start, exp=eta_exp)
         new_attack_angle, new_bank_angle=apply_action(S_t, A_t)
         sim_status=k.evolve_system(new_attack_angle, new_bank_angle, integration_steps_per_learning_step, integration_step)
         if not sim_status==0:
             R_t1 = scheduling(-300000.0/factor, i, horizon/4)
             cumulative_reward+=R_t1
-            rewards.append(cumulative_reward)
-            durations.append(i)
             target=torch.tensor(R_t1)
             l=loss(target, q[A_t])
             print("epsilon ", eps, " eta", optimizer.param_groups[0]['lr'], "Simulation failed at learning step: ", i, " reward ", cumulative_reward)
@@ -158,12 +156,10 @@ def dql_episode(k, net, optimizer, loss, initial_position, initial_velocity, win
         tensor_state[1]/=n_bank
         tensor_state[2]/=n_beta
         R_t1 = k.reward(learning_step)/factor
-        cumulative_reward+=R_t1 
+        cumulative_reward+=R_t1
         if i==int(horizon)-1:
             print("Simulation ended at learning step: ", i, " reward ", cumulative_reward)
             target=torch.tensor(R_t1)
-            rewards.append(cumulative_reward)
-            durations.append(i)
         else:
             target=R_t1+gamma*torch.max(net(tensor_state))
         l=loss(target, q[A_t])
@@ -171,276 +167,59 @@ def dql_episode(k, net, optimizer, loss, initial_position, initial_velocity, win
         optimizer.zero_grad()
         l.backward()
         optimizer.step()
-    return t,theta,phi,r
+    return cumulative_reward, i+1
 
-def dql_eval(net, horizon, integration_step, integration_steps_per_learning_step, initial_position, initial_velocity, wind):
-    theta=[]
-    phi=[]
-    r=[]
-    net.eval()
-    k=pk.kite(initial_position, initial_velocity, wind)
-    initial_beta=k.beta()
-    S_t=(14,3,initial_beta)
-    acc=k.accelerations()
-    S_t+=acc
-    for i in range(horizon):
-        tensor_state=torch.tensor(S_t).float()
-        tensor_state[0]/=n_attack
-        tensor_state[1]/=n_bank
-        tensor_state[2]/=n_beta
-        q=net(tensor_state).reshape(3,3)
-        print(torch.max(q))
-        #print(S_t[:2])
-        theta.append(k.position.theta)
-        phi.append(k.position.phi)
-        r.append(k.position.r)
-        A_t=(q==torch.max(q)).nonzero().reshape(-1)
-        A_t=A_t[0],A_t[1]
-        #print(A_t)
-        new_attack_angle, new_bank_angle=apply_action(S_t, A_t)
-        status=k.evolve_system(new_attack_angle, new_bank_angle, integration_steps_per_learning_step, integration_step)
-        if not status==0:
-            print("Simulation failed at learning step: ", i)
-            break
-        S_t = (new_attack_angle, new_bank_angle, k.beta())
-        acc=k.accelerations()
-        S_t+=acc
-        #visits[S_t]+=1
-        if i==int(horizon)-1:
-            print( "Simulation ended at learning step: ", i)
-    return theta, phi, r
-'''
-def update_weights(target_net, net):
-    for pt in target_net.named_parameters():
-        for pn in net.named_parameters():
-            if pt[0]==pn[0]:
-                pt[1].data=pn[1].data
-
-                
-                
-                
-def dql_post(fixed_net, switch, net, opt, episodes, horizon, learning_step, integration_step, integration_steps_per_learning_step, initial_pos, initial_vel, wind, lr=0.1, lr_decay_exp=0.6, lr_decay_start=200000, eps=0.01, eps_decay_exp=1.2, eps_decay_start=400000, gamma=1, plot=False):
+def sarsa(k, Q, params, initial_position, initial_velocity):
+    t=0
     durations=[]
     rewards=[]
-    losses=[]
-    if opt=='adam':
-        optimizer=torch.optim.Adam(net.parameters(), lr=lr, weight_decay=0.000)
-    else:
-        optimizer=torch.optim.SGD(net.parameters(), lr=lr, weight_decay=0.000)
-    loss=torch.nn.SmoothL1Loss()
-    t=0
-    k=pk.kite(initial_pos, initial_vel)
-    for episode in range(episodes):
-        print(episode)
-        k.reset(initial_pos, initial_vel)
-        t,theta,phi,r=dql_post_episode(k, fixed_net, switch, net, optimizer, loss, initial_pos, initial_vel, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, lr, lr_decay_exp, lr_decay_start, eps, eps_decay_exp, eps_decay_start, durations, rewards, gamma, t, plot)
-    return durations, rewards,theta,phi,r
-
-def dql_post_episode(k, fixed_net, switch, net, optimizer, loss, initial_position, initial_velocity, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, eta0, eta_decay, eta_decay_start, eps0, eps_decay, eps_decay_start, durations, rewards, gamma, t, plot):
-    if plot:
-        theta=[]
-        phi=[]
-        r=[]
-    else:
-        theta=None
-        phi=None
-        r=None
-    factor=1e5
-    cumulative_reward=0
-    initial_beta=k.beta()
-    S_t=(np.random.randint(0,n_attack), np.random.randint(0,n_bank))
-    acc=k.accelerations()
-    S_t+=acc
-    k.C_l, k.C_d = pk.coefficients[S_t[0],0], pk.coefficients[S_t[0],1]
-    k.psi = np.deg2rad(pk.bank_angles[S_t[1]])
-    tensor_state=torch.tensor(S_t).float()
-    tensor_state[0]/=n_attack
-    tensor_state[1]/=n_bank
-    #tensor_state[2]/=n_beta
-    #print(net(tensor_state).reshape(3,3))
-    for i in range(horizon):
-        if plot:
-            theta.append(k.position.theta)
-            phi.append(k.position.phi)
-            r.append(k.position.r)
-        t+=1
-        
-        eps=scheduling(eps0, t, eps_decay_start, exp=eps_decay)
-        if i<switch:
-            q=fixed_net(tensor_state).reshape(3,3)
-            A_t=(q==torch.max(q)).nonzero().reshape(-1)
-            A_t=A_t[0],A_t[1]
+    eta0=params['eta0']
+    gamma=params['gamma']
+    eps0=params['eps0']
+    eps_exp=params['eps_decay_rate']
+    eps_start=params['eps_decay_start']
+    eta_exp=params['eta_decay_rate']
+    eta_start=params['eta_decay_start']
+    episode_duration=params['episode_duration']
+    learning_step=params['learning_step']
+    horizon=int(episode_duration/learning_step)
+    integration_step=params['integration_step']
+    integration_steps_per_learning_step=int(learning_step/integration_step)
+    wind_type=params['wind_type']
+    episodes=int(params['episodes'])
+    for j in range(episodes):
+        cumulative_reward=0
+        k.reset(initial_position, initial_velocity, wind_type)
+        initial_beta=k.beta()
+        S_t=(np.random.randint(0,n_attack), np.random.randint(0,n_bank), initial_beta)
+        k.C_l, k.C_d = pk.coefficients[S_t[0],0], pk.coefficients[S_t[0],1]
+        k.psi = np.deg2rad(pk.bank_angles[S_t[1]])
+        A_t=eps_greedy_policy(Q, S_t, eps0)
+        for i in range(horizon):
+            t+=1
+            eps=scheduling(eps0, t, eps_start, exp=eps_exp)
+            eta=scheduling(eta0, t, eta_start, exp=eta_exp)
             new_attack_angle, new_bank_angle=apply_action(S_t, A_t)
             sim_status=k.evolve_system(new_attack_angle, new_bank_angle, integration_steps_per_learning_step, integration_step)
             if not sim_status==0:
-                R_t1 = scheduling(-300000.0/factor, i, horizon/4)
+                R_t1 = scheduling(-3000000, i, horizon/4)
                 cumulative_reward+=R_t1
+                print(j, "Simulation failed at learning step: ", i, " reward ", cumulative_reward)
                 rewards.append(cumulative_reward)
-                durations.append(i)
-                print("epsilon ", eps, " eta", optimizer.param_groups[0]['lr'], "Simulation failed at learning step: ", i, " reward ", cumulative_reward)
+                durations.append(i+1)
+                Q=terminal_step(Q, S_t, A_t, R_t1, eta)
                 break
-            S_t1 = (new_attack_angle, new_bank_angle)
-            acc=k.accelerations()
-            S_t1+=acc
-            tensor_state=torch.tensor(S_t1).float()
-            tensor_state[0]/=n_attack
-            tensor_state[1]/=n_bank
-            #tensor_state[2]/=n_beta
-            R_t1 = k.reward(learning_step)/factor
-            cumulative_reward+=R_t1
-            S_t=S_t1
-        else:
-            
-            if i<300:
-                eps=eps
-            else:
-                eps=eps*5
-            
-            optimizer.param_groups[0]['lr']=scheduling(eta0, t, eta_decay_start, exp=eta_decay)
-            q=net(tensor_state).reshape(3,3)
-            A_t=torch.randint(3,(2,)) if np.random.rand()<eps else (q==torch.max(q)).nonzero().reshape(-1)
-            A_t=A_t[0],A_t[1]
-            #print(q)
-            #print(A_t)
-            new_attack_angle, new_bank_angle=apply_action(S_t, A_t)
-            sim_status=k.evolve_system(new_attack_angle, new_bank_angle, integration_steps_per_learning_step, integration_step)
-            if not sim_status==0:
-                R_t1 = scheduling(-300000.0/factor, i, horizon/4)
-                cumulative_reward+=R_t1
-                rewards.append(cumulative_reward)
-                durations.append(i)
-                target=torch.tensor(R_t1)
-                l=loss(target, q[A_t])
-                print("epsilon ", eps, " eta", optimizer.param_groups[0]['lr'], "Simulation failed at learning step: ", i, " reward ", cumulative_reward)
-                optimizer.zero_grad()
-                l.backward()
-                optimizer.step()
-                #print(net(tensor_state).reshape(3,3))
-                break
-            S_t1 = (new_attack_angle, new_bank_angle)
-            #print(S_t1)
-            acc=k.accelerations()
-            S_t1+=acc
-            tensor_state=torch.tensor(S_t1).float()
-            tensor_state[0]/=n_attack
-            tensor_state[1]/=n_bank
-            #tensor_state[2]/=n_beta
-            #print("new state ", tensor_state)
-            R_t1 = k.reward(learning_step)/factor
-            cumulative_reward+=R_t1
-            if i==int(horizon)-1:
-                print("Simulation ended at learning step: ", i, " reward ", cumulative_reward)
-                target=torch.tensor(R_t1)
-                rewards.append(cumulative_reward)
-                durations.append(i)
-            else:
-                target=R_t1+gamma*torch.max(net(tensor_state))
-            l=loss(target, q[A_t])
-            S_t=S_t1
-            optimizer.zero_grad()
-            l.backward()
-            optimizer.step()
-    return t,theta,phi,r
-
-    
-    
-def dql_mem(net, opt, episodes, horizon, learning_step, integration_step, integration_steps_per_learning_step, initial_pos, initial_vel, wind, lr=0.1, lr_decay_exp=0.6, lr_decay_start=200000, eps=0.01, eps_decay_exp=0.6, eps_decay_start=400000, gamma=1, plot=False):
-    durations=[]
-    rewards=[]
-    losses=[]
-    if opt=='adam':
-        optimizer=torch.optim.Adam(net.parameters(), lr=lr, weight_decay=0.000)
-    else:
-        optimizer=torch.optim.SGD(net.parameters(), lr=lr, weight_decay=0.000)
-    loss=torch.nn.SmoothL1Loss()
-    t=0
-    memory=torch.empty((1,15))
-    k=pk.kite(initial_pos, initial_vel)
-    for episode in range(episodes):
-        print(episode)
-        k.reset(initial_pos, initial_vel)
-        t,memory,theta,phi,r=dql_episode(k, memory, net, optimizer, loss, initial_pos, initial_vel, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, lr, lr_decay_exp, lr_decay_start, eps, eps_decay_exp, eps_decay_start, durations, rewards, gamma, t, plot)
-    print(memory.size())
-    return durations, rewards,theta,phi,r
-
-
-def dql_mem_episode(k, memory, net, optimizer, loss, initial_position, initial_velocity, wind, horizon, learning_step, integration_step, integration_steps_per_learning_step, eta0, eta_decay, eta_decay_start, eps0, eps_decay, eps_decay_start, durations, rewards, gamma, t, plot):
-    if plot:
-        theta=[]
-        phi=[]
-        r=[]
-    else:
-        theta=None
-        phi=None
-        r=None
-    cumulative_reward=0
-    initial_beta=k.beta()
-    S_t=(np.random.randint(0,n_attack), np.random.randint(0,n_bank), initial_beta)
-    k.C_l, k.C_d = pk.coefficients[S_t[0],0], pk.coefficients[S_t[0],1]
-    k.psi = np.deg2rad(pk.bank_angles[S_t[1]])
-    acc=k.accelerations()
-    S_t+=acc
-    tensor_state=torch.tensor(S_t).float()
-    for i in range(horizon):
-        if plot:
-            theta.append(k.position.theta)
-            phi.append(k.position.phi)
-            r.append(k.position.r)
-        t+=1
-        eps=scheduling(eps0, t, eps_decay_start, exp=eps_decay)
-        optimizer.param_groups[0]['lr']=scheduling(eta0, t, eta_decay_start, exp=eta_decay)
-        q=net(tensor_state).reshape(3,3)
-        A_t=torch.randint(3,(2,)) if np.random.rand()<eps else (q==torch.max(q)).nonzero().reshape(-1)
-        A_tensor=A_t
-        A_t=A_t[0],A_t[1]
-        new_attack_angle, new_bank_angle=apply_action(S_t, A_t)
-        sim_status=k.evolve_system(new_attack_angle, new_bank_angle, integration_steps_per_learning_step, integration_step)
-        if not sim_status==0:
-            R_t1 = torch.tensor(scheduling(-300000.0, i, horizon/4)).reshape(1)
-            cumulative_reward+=R_t1
-            rewards.append(cumulative_reward)
-            durations.append(i)
-            print("epsilon ", eps, " eta", optimizer.param_groups[0]['lr'], "Simulation failed at learning step: ", i, " reward ", cumulative_reward)
-            fail=True
-        else:
-            fail=False
             S_t1 = (new_attack_angle, new_bank_angle, k.beta())
-            S_t1+=k.accelerations()
-            tensor_state1=torch.tensor(S_t1).float()
-            R_t1 = torch.tensor(k.reward(learning_step)).reshape(1)
-            cumulative_reward+=R_t1 
-        element=torch.cat((tensor_state, R_t1, A_tensor, tensor_state1)).reshape(1,15)
-        if memory.size()[0]<10000:
-            memory=torch.cat((memory,element))
-        else:
-            memory[torch.randint(10000, (1,))]=element
-        if i==int(horizon)-1:
-            rewards.append(cumulative_reward)
-            print(cumulative_reward, i)
-            durations.append(i)
-        if memory.size()[0]>100:
-            S,R,A,Sp=sample(memory)
-            target=torch.max(R+gamma*torch.max(net(Sp)).detach(), R)
-            #print(target, net(S))
-            #print(net(S).gather(1, A.view(-1,1)))
-            l=loss(target.reshape(32,1), net(S).gather(1, A.view(-1,1)))
+            R_t1 = k.reward(learning_step)
+            cumulative_reward+=R_t1
+            A_t1=eps_greedy_policy(Q, S_t1, eps)
+            if i==int(horizon)-1:
+                Q=terminal_step(Q, S_t, A_t, R_t1, eta)
+                print(j, "Simulation ended at learning step: ", i, " reward ", cumulative_reward)
+                rewards.append(cumulative_reward)
+                durations.append(i+1)
+            else:
+                Q=step(Q, S_t, A_t, R_t1, S_t1, A_t1, eta, gamma)
             S_t=S_t1
-            optimizer.zero_grad()
-            l.backward()
-            optimizer.step()
-        if fail:
-            break
-    return t,memory,theta,phi,r
-
-
-def sample(memory):
-    mem=memory[1:]
-    indices=torch.randint(mem.size()[0], (32,))
-    S=mem[indices,:6]
-    Sp=mem[indices,-6:]
-    R=mem[indices,6]
-    A=(torch.mul(mem[indices,7],3)+mem[indices,8]).long()
-    return S,R,A,Sp
-    
-
-'''
+            A_t=A_t1
+    return Q, durations, rewards
