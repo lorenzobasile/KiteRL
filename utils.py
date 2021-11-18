b0663a66ead29901ab98e5a0c0141286a2e2e4c1
Claudio Leone <cld.leone94@gmail.com>
2021-11-18 15:19:26 +0100

    analyze training added

--- utils.py
+++ utils.py
@@ -29,6 +29,34 @@ def write_params(param_dict, dir_path, file_name):
             f.write(k + "\t" + str(v) + "\n")
     f.close()
 
+
+def read_params(path):
+    """Read a parameter file"""
+    params = dict()
+    f = open(path, "r")
+    for l in f.readlines():
+        try:
+            params[l.split()[0]] = float(l.split()[1])
+        except ValueError:
+            if ',' not in l.split()[1]:
+                params[l.split()[0]] = l.split()[1]
+            else:
+                params[l.split()[0]] = np.array(l.split()[1].split(',')[:-1], dtype=float)
+    return params
+
+
+def read_traj(path):
+    """Read a trajectory with headers"""
+    f = open(path, "r")
+    d_traj = []
+    r_traj = []
+    #state_labels = f.readline().split()
+    for line in f.readlines():
+        d_traj.append(line.split()[0])
+        r_traj.append(line.split()[1])
+    return np.array(d_traj), np.array(r_traj, dtype="float")
+
+
 def apply_action(state, action):
     if action==(1,1):
         return state[0],state[1]
